f26cb8433c7599c75e7f68553e98c156ac8ad4c3
Jeremy Milledge <jez.milledge@gmail.com>
2020-03-08 21:30:43 +1100

    add express and facebook webhook

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,43 @@
-const getResults = require('./get-results');
+const
+  getResults = require('./get-results'),
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json());
+
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+app.post('/webhook', (req, res) => {
+  let body = req.body;
+  if (body.object === 'page') {
+    body.entry.forEach(function (entry) {
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    })
+
+    res.status(200).send('event received');
+
+  } else {
+    res.sendStatus(400);
+  }
+})
+
+app.get('/webhook', (req, res) => {
+  let VERIFY_TOKEN = process.env.LESTER_KEY;
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  if (mode && token) {
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      console.log('webhook verified');
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
+})
+
+
 
 // selectors
 const myTeam = {
@@ -13,4 +52,4 @@ const demoTeam = {
   team: 'U8 SAL'
 };
 
-getResults(demoTeam);
+// getResults(demoTeam);
